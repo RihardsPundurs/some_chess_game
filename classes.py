cdff18746c0a677c65e7da1d93c13741068a9e8a
DonkBox <79010874+DonkBox@users.noreply.github.com>
2023-02-08 22:47:05 +0200

    Added a class.py file to seperate class definitions from main.py, replaced board with sectors.

---
+++ classes.py
@@ -0,0 +1,41 @@
+class Grid
+
+# class Player(pygame.sprite.Sprite):
+#     def __init__(self):
+#         super().__init__()
+#         player_surface1 = pygame.image.load("graphics/200px-ObserverSpy.png").convert_alpha()
+#         player_surface1 = pygame.transform.scale(player_surface1,(85,155))
+#         player_surface2 = pygame.image.load("graphics/200px-ObserverSpy.png").convert_alpha()
+#         player_surface2 = pygame.transform.scale(player_surface2,(95,145))
+#         self.player_jump_surface = pygame.image.load("graphics/200px-ObserverSpy.png").convert_alpha()
+#         self.player_jump_surface = pygame.transform.scale(self.player_jump_surface,(110,120))
+#         self.player_surface_list = [player_surface1,player_surface2]
+#         self.player_index = 0
+
+#         self.image = self.player_surface_list[self.player_index]
+#         self.rect = self.image.get_rect(midbottom = (80,299))
+#         self.gravity = 0
+
+#     def player_input(self):
+#         keys = pygame.key.get_pressed()
+#         if keys[pygame.K_SPACE] and self.rect.bottom >= 300:
+#             self.gravity = -6
+
+#     def apply_gravity(self):
+#         self.gravity += 0.1
+#         self.rect.y += self.gravity
+#         if self.rect.bottom >= 300:
+#             self.rect.bottom = 300
+
+#     def animation_state(self):
+#         if self.rect.bottom < 300:
+#             self.image = self.player_jump_surface
+#         else:
+#             self.player_index += 0.02
+#             if self.player_index >= len(self.player_surface_list):self.player_index = 0
+#             self.image = self.player_surface_list[int(self.player_index)]
+
+#     def update(self):
+#         self.player_input()
+#         self.apply_gravity()
+#         self.animation_state()
--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import pygame
 from random import randint
 from sys import exit
+from classes import *
 
 pygame.init()
 screen = pygame.display.set_mode((1280, 720), )  #pygame.FULLSCREEN
@@ -9,6 +10,9 @@ clock = pygame.time.Clock()
 game_active = False
 
 test_font = pygame.font.Font(None, 50)
+
+# player = pygame.sprite.GroupSingle()
+# player.add(Player())
 
 text_surf = test_font.render("TEST", False, "White")
 grid_surf = pygame.image.load("resources/pixil-frame-0.png").convert()
@@ -21,12 +25,20 @@ while True:
       pygame.quit()
       exit()
 
+    # if game_active:
+    #     if event.type == pygame.KEYDOWN and event.key == pygame.K_SPACE:
+    #         game_active = True
+    #         start_time = pygame.time.get_ticks()
+
   keys = pygame.key.get_pressed()
   if keys[pygame.K_RETURN]:
     game_active = True
 
   if game_active:
     screen.blit(grid_surf, grid_rect)
+    # player.draw(screen)
+    # player.update()
+
   else:
     screen.blit(text_surf, (640, 360))
   pygame.display.update()
